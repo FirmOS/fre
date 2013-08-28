215c1d128aadb8109e37a68d06965b435c0f81de
Franz Schober <franz.schober@firmos.at>
2013-08-28 12:19:00 +0200

    fos_buildtools added

---
+++ fos_buildtools/fpmake_test.pas
@@ -0,0 +1,47 @@
+program fpmake_test;
+
+{$Mode objfpc}
+{$H+}
+{$inline on}
+
+ uses fpmkunit,classes,sysutils,fos_buildtools;
+
+ Var
+   P   : TPackage;
+   T   : TTarget;
+
+ begin
+   With Installer(TFOSInstaller) do begin
+     Defaults.BinInstallDir  := GetCurrentDir+'/../../bin';
+     P := AddPackage('FIRMOSDEVTESTSERVER');
+     with p do begin
+       OSes := cFOS_BUILD_OSes;
+       FOS_OS_SPEC_OPTIONS(Options);
+       with Dependencies do begin
+         Add('FRE_CORE');
+         Add('FRE_INTF');
+         Add('FRE_FCOM');
+         Add('FRE_APS');
+         Add('FRE_DB');
+         Add('FRE_APPS');
+         Add('FRE_BLKCOM');
+         Add('FRE_SYNAPSE');
+         Add('FRE_HAL');
+         Add('FOS_FIRMBOX');
+         Add('FOS_ARTEMES');
+         Add('FOS_MONSYS');
+         Add('FOS_CAPTIVEPORTAL');
+       end;
+       Directory:=cFOS_BUILD_PREFIX+'fre_test_server/';
+       with Targets do begin
+         AddUnit('../../core/fre_db/fre_dbtest.pas');
+       end;
+       InstallProgramSuffix := FOSBuild.FOS_Suffix;
+       with targets do begin
+         AddProgram('fre_testserver.lpr').ExtraEnvironment.Values['FOS_PRODUCT_NAME'] := 'FIRMOSDEV TestServer';
+         AddProgram('fre_testdatafeed.lpr').ExtraEnvironment.Values['FOS_PRODUCT_NAME'] := 'FIRMOSDEV TestFeeder';
+       end;
+     end;
+     Run;
+   end;
+ end.
