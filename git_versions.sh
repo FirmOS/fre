c4410b871c0a340f2038ea4434cbc0229568f7f0
Franz Schober <franz.schober@firmos.at>
2014-01-26 22:03:29 +0100

    git_versions.sh

---
+++ git_versions.sh
@@ -0,0 +1,26 @@
+GIT_VERSIONS=""
+
+function add_versions(){
+  NEW_VERSION=$(git rev-parse --abbrev-ref HEAD)" "$(git log -1 --format="%H [%ci]")" ---> "$(basename $(pwd))
+  GIT_VERSIONS=$(printf "$GIT_VERSIONS\n$NEW_VERSION")
+}
+
+cd ../core
+add_versions
+cd ../hal
+add_versions
+cd ../artemes
+add_versions
+cd ../citycom
+add_versions
+cd ../extra
+add_versions
+cd ../firmbox
+add_versions
+cd ../frejs
+add_versions
+cd ../monsys
+add_versions
+cd ../firmosdev
+add_versions
+printf "$GIT_VERSIONS\n"
