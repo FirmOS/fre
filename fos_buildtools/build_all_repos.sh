836f1344e357a27b798bd81c74375c3ab873ba24
Franz Schober <franz.schober@firmos.at>
2014-02-18 11:54:42 +0100

    abort condition in build_all_repos.sh test script

--- fos_buildtools/build_all_repos.sh
+++ fos_buildtools/build_all_repos.sh
@@ -6,27 +6,57 @@ export FOS_PRODUCT_MAJOR_VERSION=0
 export FOS_PRODUCT_MINOR_VERSION=8
 export FOS_PRODUCT_BUILD_NUMBER=LOCALTEST
 
-printf "$FOS_PRODUCT_BUILD_HASH"
+#printf "$FOS_PRODUCT_BUILD_HASH"
 echo
 echo "core"
 cd ../../core/fos_buildtools
 ./clean_all.sh
 ./matrix_build.sh $1
+if [ $? != 0 ] ; then
+  echo "Aborted."
+  exit 99
+fi
 echo "hal" 
 cd ../../hal/fos_buildtools
 ./matrix_build.sh $1
+if [ $? != 0 ] ; then
+  echo "Aborted."
+  exit 99
+fi
 echo "artemes"
 cd ../../artemes/fos_buildtools
 ./matrix_build.sh $1
+if [ $? != 0 ] ; then
+  echo "Aborted."
+  exit 99
+fi
 echo "firmbox"
 cd ../../firmbox/fos_buildtools
 ./matrix_build.sh $1
+if [ $? != 0 ] ; then
+  echo "Aborted."
+  exit 99
+fi
 echo "citycom"
 cd ../../citycom/fos_buildtools
 ./matrix_build.sh $1
+if [ $? != 0 ] ; then
+  echo "Aborted."
+  exit 99
+fi
 echo "monsys"
 cd ../../monsys/fos_buildtools
 ./matrix_build.sh $1
+if [ $? != 0 ] ; then
+  echo "Aborted."
+  exit 99
+fi
 echo "firmosdev"
 cd ../../firmosdev/fos_buildtools
 ./matrix_build.sh $1
+if [ $? != 0 ] ; then
+  echo "Aborted."
+  exit 99
+fi
+echo "Success!"
+
