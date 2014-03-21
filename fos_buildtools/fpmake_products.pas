739dc6d3ac4fd831bd4371e44849fb238a949a21
Franz Schober <franz.schober@firmos.at>
2014-03-21 15:11:02 +0100

    changed build, moved foscmd, fre_safejob, fre_illumos_sdparameter

--- fos_buildtools/fpmake_products.pas
+++ fos_buildtools/fpmake_products.pas
@@ -54,6 +54,28 @@ begin
         AddProgram('fre_disksubfeeder.lpr');
       end;
     end;
+    P := AddPackage('FOS_FIRMBOX_TOOLS');
+    with p do begin
+      OSes := cFOS_BUILD_OSes;
+      FOS_OS_SPEC_OPTIONS(Options);
+      with Dependencies do begin
+        Add('FRE_INTF');
+        Add('FRE_CORE');
+        Add('FRE_DB');
+        Add('FRE_HAL');
+        Add('FRE_APPS');
+        Add('fcl-xml');
+        Add('fcl-fpcunit');
+      end;
+      Directory:=cFOS_BUILD_PREFIX+'tools/';
+      Options.Add('-Fi../../core/fos_include');
+      InstallProgramSuffix := FOSBuild.FOS_Suffix;
+      with targets do begin
+        AddProgram('fre_illumos_sdparameter.lpr');
+        AddProgram('fre_safejob.lpr');
+        AddProgram('foscmd.lpr');
+      end;
+    end;
     Run;
   end;
 end.
