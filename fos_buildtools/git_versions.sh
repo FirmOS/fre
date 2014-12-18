20b7215ff1a29ed5f3a49dd59e587d76ce56b617
Franz Schober <franz.schober@firmos.at>
2014-12-18 10:02:10 +0100

    changed git versions to FOSSV_0.0.0.0_FOSEV

--- fos_buildtools/git_versions.sh
+++ fos_buildtools/git_versions.sh
@@ -1,6 +1,6 @@
 GIT_VERSIONS=""
 
-NEW_VERSION=$(git reflog -1 --format="%d" | sed -e 's/.*tag: //; s/,.*//')
+NEW_VERSION=$(git reflog -1 --format="%d" | sed -e 's/.*FOSSV_//; s/_FOSEV.*//')
 NEW_HASH=$(git log -1 --format="%h [%ci]")
 GIT_VERSIONS="$NEW_VERSION"" ""$NEW_HASH"
 
